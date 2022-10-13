034cde487a3d1e732285c87ddcc6cfa6f51e5fd2
rfmartins0 <francomartins@gmail.com>
2022-10-13 01:08:44 -0300

    Correção

--- system/auth/src/main/java/br/com/auth/exception/ErrorHandler.java
+++ system/auth/src/main/java/br/com/auth/exception/ErrorHandler.java
@@ -1,17 +1,12 @@
 package br.com.auth.exception;
 
-import com.fasterxml.jackson.core.JsonParseException;
-import com.google.gson.Gson;
-import io.jsonwebtoken.SignatureException;
 import org.springframework.http.HttpStatus;
-import org.springframework.validation.FieldError;
-import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
-import java.util.ArrayList;
+import io.jsonwebtoken.SignatureException;
 
 @ControllerAdvice
 public class ErrorHandler {
